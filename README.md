e3e18bf0087d42836ad7b6f3a02ea65fd61cfa00
AndriiVasylenkoML <livekidos95@gmail.com>
2025-02-10 22:40:48 +0200

    Update main.py

--- README.md
+++ README.md
@@ -1,5 +1,50 @@
 # MongoDB Crash Course With Python 2022
 MongoDB is a user-friendly and popular document-oriented database that allows users to easily manage data in collections of documents. 
+---
+
+1. Start MongoDB.
+You can start the mongod process by issuing the following command:
+```
+sudo systemctl start mongod
+```
+If you receive an error similar to the following when starting mongod:
+
+Failed to start mongod.service: Unit mongod.service not found.
+
+Run the following command first:
+```
+sudo systemctl daemon-reload
+```
+Then run the start command above again.
+
+2. Verify that MongoDB has started successfully.
+sudo systemctl status mongod
+
+You can optionally ensure that MongoDB will start following a system reboot by issuing the following command:
+```
+sudo systemctl enable mongod
+```
+3. Stop MongoDB.
+As needed, you can stop the mongod process by issuing the following command:
+```
+sudo systemctl stop mongod
+```
+4. Restart MongoDB.
+You can restart the mongod process by issuing the following command:
+```
+sudo systemctl restart mongod
+```
+You can follow the state of the process for errors or important messages by watching the output in the /var/log/mongodb/mongod.log file.
+
+5. Begin using MongoDB.
+Start a mongosh session on the same host machine as the mongod. You can run mongosh without any command-line options to connect to a mongod that is running on your localhost with default port 27017.
+```
+mongosh
+```
+For more information on connecting using mongosh, such as to connect to a mongod instance running on a different host and/or port, see the mongosh documentation.
+
+To help you start using MongoDB, MongoDB provides Getting Started Guides in various driver editions. For the driver documentation, see Start Developing with MongoDB.
+
 
 Sources:
 1. MongoDB Crash Course With Python 2022. YouTube: https://youtu.be/qWYx5neOh2s?si=RuQ0WUIEneR5FvUu
--- main.py
+++ main.py
@@ -19,12 +19,47 @@ print(db.list_collection_names())
 todo1 = {"name": "Patrick", "text": "My first todo!", "status": "open",
          "tags": ["python", "coding"], "date": datetime.datetime.utcnow()}
 
-todo2 = [{"name": "Patrick", "text": "My second todo!", "status": "open",
-          "tags": ["python", "coding"], "date": datetime.datetime.utcnow()}, {"name": "Mary", "text": "My third todo!", "status": "open", "tags": ["python", "coding"], "date": datetime.datetime(2021, 1, 1, 10, 45)}]
+todos2 = [{"name": "Patrick", "text": "My second todo!", "status": "open",
+          "tags": ["python", "coding"], "date": datetime.datetime.utcnow()}, {"name": "Mary", "text": "My third todo!", "status": "open", "tags": ["c++", "coding"], "date": datetime.datetime(2021, 1, 1, 10, 45)}]
 
 todos = db.todos
 
 # result = todos.insert_one(todo1)
-result = todos.insert_many(todo2)
+# result = todos.insert_many(todos2)
+# result = todos.find_one({"name": "Patrick"})
+# result = todos.find_one({"tags": "c++"})
 
-print(result)
+# Dont work.
+# result = todos.find_one({"_id":  "67a9f76e78e7c80bd98b1a5f"})
+# from bson.objectid import ObjectId
+# result = todos.find_one({"_id": ObjectId("67a9f76e78e7c80bd98b1a5f")})
+# print(result)
+
+# Pymongo cursor object
+# results = todos.find({"name": "Patrick"})
+# print(results)
+# for result in results:
+#     print(result)
+
+
+# print(todos.count_documents({"tags": "c++"}))
+# print(todos.count_documents({"tags": "c#"}))
+
+
+# d = datetime.datetime(2021, 2, 1)
+# results = todos.find({"date": {"$lt": d}}) # $lt = less than (менше ніж)
+# results = todos.find({"date": {"$gt": d}}) # $gt = greater than (більше ніж)
+# results = todos.find({"date": {"$gt": d}}).sort("name")
+# for result in results:
+#     print(result)
+
+## Delete data.  
+# from bson.objectid import ObjectId
+# result = todos.delete_one({"_id": ObjectId("67a9f76e78e7c80bd98b1a5f")})
+# result = todos.delete_many({"name": "Patrick"})
+# Delete all data.
+# result = todos.delete_many({})
+
+# result = todos.update_one({"tags": "c++"}, {"$set": {"status": "done"}})
+# result = todos.update_many({"tags": "c++"}, {"$unset": {"status": None}})
+result = todos.update_many({"tags": "c++"}, {"$set": {"status": "new open"}})
